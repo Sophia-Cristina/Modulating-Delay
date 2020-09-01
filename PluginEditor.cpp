b27adac663ec17426586159181009d18a8c42a62
Sophia-Cristina <43291594+Sophia-Cristina@users.noreply.github.com>
2020-09-01 02:30:58 -0300

    Update PluginEditor.cpp

--- PluginEditor.cpp
+++ PluginEditor.cpp
@@ -25,6 +25,11 @@ NewProjectAudioProcessorEditor::NewProjectAudioProcessorEditor(NewProjectAudioPr
     // ADD COMPONENTS HERE:
 
     addAndMakeVisible(MainSliders);
+    MainSliders.MSSlider.setValue(166.67);
+    MainSliders.FeedBack.setValue(0.75);
+    MainSliders.Floor.setValue(1.0);
+    MainSliders.Multiplier.setValue(0.5);
+    MainSliders.Omega.setValue(1.0);
     MainSliders.MSSlider.addListener(this);
     MainSliders.FeedBack.addListener(this);
     MainSliders.Floor.addListener(this);
@@ -68,8 +73,8 @@ void NewProjectAudioProcessorEditor::paint(juce::Graphics& g)
     //g.drawText("SampleRate: " + to_string(audioProcessor.Delay.SampleRate), 10, 520, getWidth(), 30, juce::Justification::centredBottom, true);
     g.drawText("MSNum: " + to_string(audioProcessor.Delay.MSNum), 10, 520, getWidth(), 30, juce::Justification::centredBottom, true);
     g.drawText("DelayBuffer Num. Samples: " + to_string(audioProcessor.Delay.DelayBuffer.getNumSamples()), 10, 490, getWidth(), 30, juce::Justification::centredBottom, true);
-    g.drawText("Delay Buffer Size: " + to_string(audioProcessor.Delay.DelayBFDataSizeView), 10, 460, getWidth(), 30, juce::Justification::centredBottom, true);
     g.setColour({ 255, 0, 0 });
+    g.drawText("Cont Table Index: " + to_string(audioProcessor.Delay.CntTable / (audioProcessor.Delay.SampleRate / audioProcessor.Delay.Omega)), 10, 460, getWidth(), 30, juce::Justification::centredBottom, true);
     g.drawText("WRITE POS: " + to_string(audioProcessor.Delay.WritePos), 10, 430, getWidth(), 30, juce::Justification::centredBottom, true);
     g.setOpacity(1.0f);
 }
@@ -78,6 +83,7 @@ void NewProjectAudioProcessorEditor::resized()
 {
     //MainSliders.setBounds(10, 200, SldX.getX(), 50);
     MainSliders.setBounds(10, 10, 280, 360);
+    //TableScope.setBounds(10, 380, 280, 360); // Not working yet
 
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
